3bbf0b0dbb24b7bd8a81bd0b27cb453d7190c4b0
Ian Pye <ipye@zink.(none)>
2009-06-04 14:11:04 +0300

    closer to the actual site

---
+++ ushahidi/application/views/sidebar.php
@@ -0,0 +1,44 @@
+										  <h3>Users Area</h3>
+										<h2>Subscribe</h2>
+
+										  <div class ="sharek_ul">
+										  <ul>
+											<li><a href ="http://www.sharek961.org/feed">RSS Feed</a></li>
+											<li><a href ="http://twitter.com/sharek961" TARGET=_BLANK>Twitter</a></li>
+											<li><a href ="http://www.facebook.com/pages/Sharek961/100789857944?ref=mf" TARGET=_BLANK>Facebook</a></li>
+
+
+										  </ul>
+										  </div>
+										<h2>About</h2>
+										  <div class ="sharek_ul">
+										  <ul>
+											<li><a href ="http://www.sharek961.org/sharikblog/?page_id=2">About Us</a></li>
+                                            <li><a href ="http://www.sharek961.org/sharikblog/?page_id=5">Sharek961 Team</a></li>
+                                            <li><a href ="http://www.sharek961.org/sharikblog/?page_id=7">Partners</a></li>
+                                            <li><a href="http://www.sharek961.org/sharikblog/?page_id=9">FAQ</a><li>
+                                            <li><a href="http://www.sharek961.org/sharikblog/?page_id=11">Get Involved</a></li>
+                                            <li><a href="http://www.sharek961.org/sharikblog/?page_id=13">How to Report</a></li>
+                                            <li><a href= "http://www.sharek961.org/sharikblog/?page_id=25">Spread the Word</a></li>
+											
+										  </ul>
+										  </div>
+										
+                                        <h2>Press Relations</h2>
+										  <div class ="sharek_ul">
+										  <ul>
+											<li><a href ="http://www.sharek961.org/sharikblog/?page_id=73">Contact Us</a></li>
+                                            <li><a href="http://www.sharek961.org/sharikblog/?page_id=17">Press Room</a></li>
+                                            <li><a href="http://www.sharek961.org/sharikblog/?page_id=18">Press Release</a></li>
+                                            <li><a href ="#">Syndicate Us</a></li>		
+										  </ul>
+										  </div>
+										
+                                        <h2>Organizations</h2>
+										  <div class ="sharek_ul">
+										  <ul>
+											<li><a href ="#">Register With Us</a></li>
+											<li><a href ="#">Report The Field</a></li>
+
+										  </ul>
+										  </div>
